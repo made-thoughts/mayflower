9bccee72a683c6e4e72bc1a372eb3965fbcd7d5c
Nick Hensel <nickhensel25@icloud.com>
2022-12-26 00:50:43 +0100

    feat: Add micronaut and paper dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
 plugins {
     java
+    id("io.micronaut.library") version "3.5.1"
+}
+
+group = "io.github.madethougth"
+version = "0.1"
+description = "mayflower"
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+    mavenCentral()
+}
+
+dependencies {
+    annotationProcessor("io.micronaut:micronaut-inject-java")
+
+    implementation("io.micronaut", "micronaut-inject-java")
+    implementation("io.micronaut", "micronaut-runtime")
+
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
 }
 
 java {
