6dc8599f1a7845af02ccd81285f49faeaeb78ffb
Nick Hensel <nickhensel25@icloud.com>
2022-12-26 01:09:12 +0100

    feat: Add mayflower plugin class

---
+++ src/main/java/io/github/madethougths/mayflower/MayflowerPlugin.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.madethougths.mayflower;
+
+import io.micronaut.context.ApplicationContext;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MayflowerPlugin extends JavaPlugin {
+
+    private final ApplicationContext applicationContext;
+
+    public MayflowerPlugin() {
+        this.applicationContext = ApplicationContext.run(this.getClassLoader()).start();
+        applicationContext.registerSingleton(this);
+        applicationContext.registerSingleton(this.getServer());
+    }
+
+    @Override
+    public void onDisable() {
+        applicationContext.close();
+    }
+}
