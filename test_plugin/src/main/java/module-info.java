15173fb839936af69aa1de2975cb0fd085da74fb
Nick Hensel <nickhensel25@icloud.com>
2023-01-14 15:53:45 +0100

    feat: Add `module-info.java` files for mayflower and test_plugin modules

---
+++ test_plugin/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module mayflower.test.plugin.main {
+    requires java.logging;
+
+    requires org.bukkit;
+    requires io.github.madethoughts.mayflower;
+
+    //    requires io.micronaut.inject;
+//    requires io.micronaut.aop;
+    requires io.micronaut.context;
+}
